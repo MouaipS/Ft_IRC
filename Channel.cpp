3c75e00fcbefc7990f7ca6a61cbd0c556f46365e
Nika Papashvili <npapashv@3D3.42angouleme.fr>
2025-10-27 16:08:47 +0100

    WIP function descriptions

--- Channel.cpp
+++ Channel.cpp
@@ -107,28 +107,50 @@ void Channel::setIsLimit(bool value)
 
 // ------------- UTILS  ------------- //
 
-size_t	Channel::findUser(User& user)
+/**
+ * @brief Recherche un utilisateur dans la liste des utilisateurs du canal.
+ * 
+ * @param user Référence vers l'utilisateur à rechercher.
+ * @return size_t 
+ * - L'indice (position) de l'utilisateur dans le vecteur `_users` s'il est trouvé.
+ * - Retourne `-1` si l'utilisateur n'est pas présent dans le canal.
+ */
+size_t Channel::findUser(User& user)
 {
 	for (size_t i = 0; i < _users.size(); ++i)
 	{
 		if (&user == _users[i])
 			return (i);
 	}
-
 	return (-1);
 }
 
-size_t	Channel::findOperator(User& user)
+/**
+ * @brief Recherche un utilisateur dans la liste des opérateurs du canal.
+ * 
+ * @param user Référence vers l'utilisateur à rechercher.
+ * @return size_t 
+ * - L'indice (position) de l'utilisateur dans le vecteur `_operators` s'il est trouvé.
+ * - Retourne `-1` si l'utilisateur n'est pas opérateur du canal.
+ */
+size_t Channel::findOperator(User& user)
 {
 	for (size_t i = 0; i < _operators.size(); ++i)
 	{
 		if (&user == _operators[i])
 			return (i);
 	}
-
 	return (-1);
 }
 
+/**
+ * @brief Ajoute un utilisateur au canal si possible.
+ * 
+ * @param user Référence vers l'utilisateur à ajouter.
+ * @return bool 
+ * - `true` si l'utilisateur a été ajouté avec succès.
+ * - `false` si l'utilisateur est déjà présent ou si la limite d'utilisateurs est atteinte.
+ */
 bool Channel::addUserToChannel(User& user)
 {
 	if (_isUserLimit && _users.size() >= _userLimit)
@@ -138,14 +160,21 @@ bool Channel::addUserToChannel(User& user)
 		return (false);
 
 	_users.push_back(&user);
-
 	return (true);
 }
 
+/**
+ * @brief Retire un utilisateur du canal (et de la liste des opérateurs s'il en fait partie).
+ * 
+ * @param user Référence vers l'utilisateur à retirer.
+ * @return bool 
+ * - `true` si l'utilisateur a été retiré avec succès.
+ * - `false` si l'utilisateur n'était pas présent dans le canal.
+ */
 bool Channel::removeUserFromChannel(User& user)
 {
-	size_t	user_index;
-	size_t	operator_index;
+	size_t user_index;
+	size_t operator_index;
 
 	user_index = findUser(user);
 	operator_index = findOperator(user);
@@ -162,6 +191,14 @@ bool Channel::removeUserFromChannel(User& user)
 	return (false);
 }
 
+/**
+ * @brief Promeut un utilisateur au rôle d'opérateur du canal.
+ * 
+ * @param user Référence vers l'utilisateur à promouvoir.
+ * @return bool 
+ * - `true` si l'utilisateur a été promu avec succès.
+ * - `false` si l'utilisateur n'existe pas dans le canal ou est déjà opérateur.
+ */
 bool Channel::promoteUser(User &user)
 {
 	if (findUser(user) < 0)
@@ -171,13 +208,20 @@ bool Channel::promoteUser(User &user)
 		return (false);
 
 	_operators.push_back(&user);
-
 	return (true);
 }
 
+/**
+ * @brief Rétrograde un opérateur en simple utilisateur.
+ * 
+ * @param user Référence vers l'utilisateur à rétrograder.
+ * @return bool 
+ * - `true` si l'utilisateur a été retiré de la liste des opérateurs.
+ * - `false` si l'utilisateur n'existe pas ou n'était pas opérateur.
+ */
 bool Channel::demoteUser(User &user)
 {
-	size_t	operator_index;
+	size_t operator_index;
 
 	if (findUser(user) < 0)
 		return (false);
