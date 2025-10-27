3227de5decad5f21c7e787255c1a64203cb90bef
adrouin <amaury.d.pro@proton.me>
2025-10-27 17:48:26 +0100

    User done

--- User.cpp
+++ User.cpp
@@ -1,17 +1,61 @@
 #include "User.hpp"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+User::User() : _isAuthed(false){}
 
 User::~User() {}
 
-/**
- * @return _username of the user
- */
 std::string User::getUsername() const {
 	return(_username);
 }
 
-/**
- * @return _nickname of the user
- */
 std::string User::getNickname() const {
 	return(_nickname);
+}
+
+bool User::getIsAuthed() const {
+	return(_isAuthed);
+}
+
+void User::setIsAuthed() {
+	_isAuthed = true;
+}
+
+void User::setNickname(std::string nickname) {
+	_nickname = nickname;
+}
+
+void User::setUsername(std::string username) {
+	_username = username;
+}
+
+bool isAlphaNum(std::string name) {
+	for(int i=0; i != name.length(); i++) {
+		if(!isalnum(name[i]))
+			return(false);
+	}
+	return(true);
+}
+
+bool isCmdName(std::string name) {
+	std::string arr[] = {"KICK", "JOIN", "PASS", "MODE", "INVITE", "PRIVMSG", "MSG", "NICK"};
+
+	for(int i = 0; i != arr->length(); i++) {
+		if(arr[i] == name)
+			return(false);
+	}
+	return (true);
+}
+
+bool User::isUsernameValid(std::string name) {
+	if(name.empty() || !isAlphaNum(name) || !isCmdName(name)) {return false;}
+	return true;
+}
+
+bool User::isNicknameValid(std::string name) {
+	if(name.empty() || !isAlphaNum(name) || !isCmdName(name)) {return false;}
+	return true;
 }
--- User.hpp
+++ User.hpp
@@ -7,24 +7,26 @@
 class User {
 
 	public :
-			User();
-			~User();
+		User();
+		~User();
 
 		//Geter
 		std::string getUsername() const;
 		std::string getNickname() const;
+		bool 		getIsAuthed() const;
 
 		//Setter
 		void setNickname(std::string nickname);
 		void setUsername(std::string Username);
+		void setIsAuthed();
 
 		// Verification
-		bool isNicknameValid(std::string value);
 		bool isUsernameValid(std::string value);
+		bool isNicknameValid(std::string value);
 		
 	private :
 
-		bool isAuthed;
+		bool _isAuthed;
 		std::string _username;
 		std::string _nickname;
 };
