14c685a1fb3621b12cb3deecd3dc4b3480b493e1
Nika Papashvili <npapashv@3C6.42angouleme.fr>
2025-10-29 17:01:51 +0100

    INIT cmdPass

--- Commands/CmdPass.cpp
+++ Commands/CmdPass.cpp
@@ -6,8 +6,19 @@ void CmdPass::execCmd(
     const std::string& name,
     const std::string& password,
     std::vector<Channel>& allChannels,
-    std::map<int, User*>& fdToUser
-) {
+    std::map<int, User*>& fdToUser)
+{
+	User* user = fdToUser[fd_origin];
+
+	if (cmd.size() != 2)
+		
+
+	if (user->getIsAuthed())
+	{
+
+	}
+	
+
     // TODO: Implémenter la commande Pass
 	(void) fd_origin;
 	(void) cmd;
