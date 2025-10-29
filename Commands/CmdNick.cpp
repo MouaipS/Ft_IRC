db10082fb5f2b0eca3cd297eca026fbd2415669a
adrouin <amaury.d.pro@proton.me>
2025-10-30 00:57:56 +0100

    Opti CmdUser &CmdNick

--- Commands/CmdNick.cpp
+++ Commands/CmdNick.cpp
@@ -1,12 +1,11 @@
 #include "CmdNick.hpp"
 
-bool isAvailable(std::string name, User *user, std::map<int, User*>& fdToUser) {
-    if(user->isNicknameValid(name) == true) {
+bool isAvailable(std::string CorrectNick, User *user, std::map<int, User*>& fdToUser) {
+    if(user->isNicknameValid(CorrectNick) == true) {
         std::map<int, User*>::iterator it;
         for(it=fdToUser.begin(); it != fdToUser.end(); it++){
-            if(name == it->second->getNickname()) {
+            if(CorrectNick == it->second->getNickname())
                 return(false);
-            }
         }
         return true;
     }
@@ -14,45 +13,30 @@ bool isAvailable(std::string name, User *user, std::map<int, User*>& fdToUser) {
 }
 
 void CmdNick::execCmd(
-    int fd_origin,
-    std::vector<std::string>& cmd,
-    const std::string& servername,
-    const std::string& password,
-    std::vector<Channel>& allChannels,
-    std::map<int, User*>& fdToUser
-) {
+    int fd_origin, std::vector<std::string>& cmd,
+    const std::string& servername, const std::string& password,
+    std::vector<Channel>& allChannels,std::map<int, User*>& fdToUser)
+{
     (void) allChannels;
     (void) password;
-    if(isAvailable(cmd[1], fdToUser[fd_origin], fdToUser) == false) {  
-        //Si Username not available ou pas isalphanum : 461 ERR_NEEDMOREPARAMS "<client> <command> :Not enough parameters" 
-        std::string message = ":";
-        message = message + servername + " 461 " + fdToUser[fd_origin]->getNickname() + " :Not enough parameters" + fdToUser[fd_origin]->getNickname() + "\r\n";
-        std::cout << "test 1" << std::endl;
-        ICommand::sendToUser(fd_origin, message, 0);
+    User *user = fdToUser[fd_origin];
+
+    if(cmd.size() < 2){
+        sendToUser(fd_origin, "431 * :No nickname given", 0);
+        return;
     }
-    else if (ICommand::isUserValidAuth(*fdToUser[fd_origin], 1, 0, 1)) {
-        //PASS 1 NICK 0 USER 1 -> Code "001" MSG ":Welcome to the IRC Network"
-        fdToUser[fd_origin]->setNickname(cmd[1]);
-        std::string message = ":";
-        message = message + servername + " 001 " + servername + " :Welcome to the IRC Network " + fdToUser[fd_origin]->getNickname() + "\r\n";  //001 RPL_WELCOME
-        std::cout << "test 2" << std::endl;
-        ICommand::sendToUser(fd_origin, message, 0);
+    std::string CorrectNick = cmd[1];
+    if(isAvailable(CorrectNick, user, fdToUser) == false) {  
+        sendToUser(fd_origin, "433 * " + CorrectNick + " :Nickname is already in use", 0);
     }
-    else if(fdToUser[fd_origin]->getIsAuthed() == false) {
-        //PASS 0 NICK 0 USER 0 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        //PASS 0 NICK 1 USER 0 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        //PASS 0 NICK 0 USER 1 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        //PASS 0 NICK 1 USER 1 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        std::string message = ":";
-        message = message + servername + " 464 " + fdToUser[fd_origin]->getNickname() + " :NERR_PASSWDMISMATCH" + fdToUser[fd_origin]->getNickname() + "\r\n";
-        std::cout << "test 3" << std::endl;
-        ICommand::sendToUser(fd_origin, message, 0);
+    else if(user->getIsAuthed() == false) {
+        sendToUser(fd_origin, "464 " + user->getNickname() + " :Password incorrect", 0);
     }
-    else {
-        //PASS 1 NICK 0 USER 0 -> RIEN (ON CHANGE SIMPLEMENT NICKAME)
-        fdToUser[fd_origin]->setNickname(cmd[1]);
-        std::cout << "test 5" << std::endl;
+    else if (isUserValidAuth(*user, 1, 0, 1)) {
+        user->setNickname(cmd[1]);
+        sendToUser(fd_origin, "001 " + CorrectNick + " :Welcome to the IRC Network", 0);
     }
+    else {user->setNickname(cmd[1]);}
 }
 
 
--- Commands/CmdUser.cpp
+++ Commands/CmdUser.cpp
@@ -13,60 +13,31 @@ bool isAvailable(std::string name, User *user, std::map<int, User*>& fdToUser) {
 }
 
 void CmdUser::execCmd(
-    int fd_origin,
-    std::vector<std::string>& cmd,
-    const std::string& servername,
-    const std::string& password,
-    std::vector<Channel>& allChannels,
-    std::map<int, User*>& fdToUser
-) {
+    int fd_origin, std::vector<std::string>& cmd,
+    const std::string& servername, const std::string& password,
+    std::vector<Channel>& allChannels, std::map<int, User*>& fdToUser)
+{
     (void) allChannels;
     (void) password;
-    if(isAvailable(cmd[1], fdToUser[fd_origin], fdToUser) == false) {  
-        //Si Username not available ou pas isalphanum : 461 ERR_NEEDMOREPARAMS "<client> <command> :Not enough parameters" 
-        std::string message = ":";
-        message = message + servername + " 461 " + fdToUser[fd_origin]->getUsername() + " :Not enough parameters" + fdToUser[fd_origin]->getUsername() + "\r\n";
-        std::cout << "test 1" << std::endl;
-        ICommand::sendToUser(fd_origin, message, 0);
+    User *user = fdToUser[fd_origin];
+
+    if (cmd.size() < 2) {
+        sendToUser(fd_origin, "431 * :No username given", 0);
+        return;
     }
-    else if (ICommand::isUserValidAuth(*fdToUser[fd_origin], 1, 1, 0)) {
-        //PASS 1 NICK 1 USER 0 -> Code "001" MSG ":Welcome to the IRC Network"
-        fdToUser[fd_origin]->setUsername(cmd[1]);
-        std::string message = ":";
-        message = message + servername + " 001 " + servername + " :Welcome to the IRC Network " + fdToUser[fd_origin]->getUsername() + "\r\n";  //001 RPL_WELCOME
-        std::cout << "test 2" << std::endl;
-        ICommand::sendToUser(fd_origin, message, 0);
+    std::string correctUser = cmd[1];
+    if (isUserValidAuth(*user, 1, 1, 1)) {
+        sendToUser(fd_origin, "462 " + user->getUsername() + " :You may not reregister", 0);
     }
-    else if(fdToUser[fd_origin]->getIsAuthed() == false) {
-        //PASS 0 NICK 0 USER 0 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        //PASS 0 NICK 1 USER 0 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        //PASS 0 NICK 0 USER 1 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        //PASS 0 NICK 1 USER 1 -> Code "464" MSG "ERR_PASSWDMISMATCH"
-        std::string message = ":";
-        message = message + servername + " 464 " + fdToUser[fd_origin]->getUsername() + " :NERR_PASSWDMISMATCH" + fdToUser[fd_origin]->getUsername() + "\r\n";
-        std::cout << "test 3" << std::endl;
-        ICommand::sendToUser(fd_origin, message, 0);
+    else if (isAvailable(correctUser, user, fdToUser) == false) {  
+        sendToUser(fd_origin, "461 * :Not enough parameters", 0);
     }
-    else if(ICommand::isUserValidAuth(*fdToUser[fd_origin], 1, 0, 1) || ICommand::isUserValidAuth(*fdToUser[fd_origin], 1, 1, 1)){
-        //PASS 1 NICK 0 USER 1 -> Code "462" MSG "ALREADY REGISTERED"
-        //PASS 1 NICK 1 USER 1 -> Code "462" MSG "ALREADY REGISTERED" 
-        std::string message = ":";
-        message = message + servername + " 462 " + fdToUser[fd_origin]->getUsername() + " :ALREADY REGISTERED" + fdToUser[fd_origin]->getUsername() + "\r\n";
-        ICommand::sendToUser(fd_origin, message, 0);
-        std::cout << "test 4" << std::endl;
+    else if (user->getIsAuthed() == false) {
+        sendToUser(fd_origin,"464 * :Password incorrect", 0);
     }
-    else {
-        //PASS 1 NICK 0 USER 0 -> RIEN (ON CHANGE SIMPLEMENT USERNAME)
-        fdToUser[fd_origin]->setUsername(cmd[1]);
-        std::cout << "test 5" << std::endl;
+    else if (isUserValidAuth(*user, 1, 1, 0)) {
+        user->setUsername(correctUser);
+        sendToUser(fd_origin, "001 " + correctUser + " :Welcome to the IRC Network", 0);
     }
+    else {user->setUsername(correctUser);}
 }
-
-
-
-
-
-
-
-
-
