74a1e107b21cd97788c6b5b2569ab371418e3e11
Joan Nauroy <jnauroy@3B3.42angouleme.fr>
2025-11-08 17:04:54 +0100

    privmsgCmd fini sauf oubli de ma part

--- Commands/CmdPrivmsg.cpp
+++ Commands/CmdPrivmsg.cpp
@@ -11,43 +11,63 @@ void CmdPrivmsg::execCmd(
 ) {
 	(void) password;
 	User*	user = fdToUser[fd_origin];
-	if (!isUserValidAuth(*user, 1, 1, 1))
-	{
+
+	// checking the manda security (authorisation and command requested size)
+	if (!isUserValidAuth(*user, 1, 1, 1)) {
 		sendToUser(fd_origin, "464 :Password incorrect", 0);
 		return ;
 	}
-	
+	if (cmd.size() < 3) {
+		sendToUser(fd_origin, "412 :No text to send", 0);
+		return ;
+	}
+
+	//writing the full message to send
 	std::string mail;
 	for (size_t i = 2; i < cmd.size(); i++)
 	{
 		mail += cmd[i];
 		if (i == cmd.size() - 1)
-		{
-			mail += "\n\f";
 			break ;
-		}
 		mail += ' ';
 	}
+
+	// because you can't say if the target is a channel or a nickname,
+	// I start by checking the channels first
 	for (size_t i = 0; i < allChannels.size(); i++) {
 		
 		if (cmd[1] == allChannels[i]->getName()) {
 			
-			std::vector<User*>	myUser = allChannels[i]->getUsers();
-			for (size_t j = 0; j < myUser.size(); j++)
-				send(myUser[j]->getFd(), mail.c_str(), mail.length(), 0);
-				// sendToUser(myUser[j]->getFd(), cmd[2], 0);
-			return ;
+			// channel good but check if user is on the channel
+			if (allChannels[i]->findUser(user->getUsername()))
+			{
+				// send the message to all of the users on the channel
+				std::vector<User*>	myUser = allChannels[i]->getUsers();
+				for (size_t j = 0; j < myUser.size(); j++)
+					sendToUser2(myUser[j]->getFd(), user->getNickname() + "!" + user->getUsername() + "@", " PRIVMSG " + cmd[1] + " :" + mail, 0);
+				return ;
+			}
+			// otherwise he can't send the message
+			else
+			{
+				sendToUser(fd_origin, "405 " + cmd[1] + " :Cannot send to channel", 0);
+				return ;
+			}
 		}
 	}
 
+	// if the target is not a channel, it must be a nickname
 	std::map<int, User*>::iterator	it = fdToUser.begin();
 	for (; it != fdToUser.end(); it++) {
 
-		if (it->second->getUsername() == cmd[1])
+		if (it->second->getNickname() == cmd[1])
 		{
-			send(it->second->getFd(), mail.c_str(), mail.length(), 0);
-			// sendToUser(it->second->getFd(), cmd[2], 0);
+			// send the message to the user
+			sendToUser2(it->second->getFd(), user->getNickname() + "!" + user->getUsername() + "@", " PRIVMSG " + cmd[1] + " :" + mail, 0);
 			return ;
 		}
 	}
+
+	// the target don't exist
+	sendToUser(fd_origin, "401 " + cmd[1] + " :No such Nick/Channel", 0);
 }
