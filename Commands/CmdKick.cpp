446cd1a6b6f6ba7b875882972b026c1fbdaab9d4
Joan Nauroy <jnauroy@3E6.42angouleme.fr>
2025-11-16 18:54:08 +0100

    kick messages ok

--- Commands/CmdKick.cpp
+++ Commands/CmdKick.cpp
@@ -13,31 +13,39 @@ CmdKick::CmdKick() : ICommand::ICommand() {};
  * - 3 : Can't find the channel
  * - 4 : Is op
  */
-int findTarget(std::string target, std::string channel, std::vector<Channel*>& allChannels, User *&userTarget) {
+
+std::vector<Channel*>::iterator	findChannel(std::vector<Channel*>& allChannels, std::string channel) {
+
     std::vector<Channel*>::iterator it = allChannels.begin();
-    for(; it != allChannels.end(); it++){
+
+    for (; it != allChannels.end(); it++) {
         if((*it)->getName() == channel)
-        {
-            if((*it)->getUsers().empty()) return(0);
-            userTarget = (*it)->findUser(target);
-            if(userTarget == NULL)
-                return(2);
-            if((*it)->findOperator(*userTarget) != -1)
-                return(4);
-            return(1);
-        }
-    }
-    return(3);
+			break ;
+	}
+	return it;
 }
 
-void eraseUser(std::string channel, std::vector<Channel*>& allChannels, User &userTarget){
-    std::vector<Channel*>::iterator it = allChannels.begin();
-    for(; it != allChannels.end(); it++){
-        if((*it)->getName() == channel){
-            (*it)->removeUserFromChannel(userTarget);
-            return;
-        }
-    }
+int findTarget(std::string target, std::vector<Channel*>::iterator &it, User *&userTarget) {
+
+	if ((*it)->getUsers().empty())
+		return(0);
+
+	userTarget = (*it)->findUser(target);
+	if (userTarget == NULL)
+		return(2);
+	if ((*it)->findOperator(*userTarget) != -1)
+		return(4);
+	return(1);
+}
+
+void	sendToUsers(std::vector<Channel*>::iterator &it, User* user, std::string who, std::string reason) {
+
+	std::vector<User*>	allUsers = (*it)->getUsers();
+
+	for (size_t i = 0; i < allUsers.size(); i++) {
+
+        clientReply(allUsers[i]->getFd(), user->getNickname(), user->getUsername(), "KICK", who, reason, 0);
+	}
 }
 
 void CmdKick::execCmd(
@@ -49,33 +57,41 @@ void CmdKick::execCmd(
 ) {
     (void)password;
     std::string reason;
+
     User *user = fdToUser[fd_origin];
-    if(!isUserValidAuth(*user, 1,1,1))
+    if (!isUserValidAuth(*user, 1,1,1))
 		throw ExceptionCode(ERR_NOTREGISTERED);
-    if(cmd.size() == 4) {
+    if (cmd.size() == 4)
         reason = cmd[3];
-    } else if(cmd.size() == 3) {
-        reason = cmd[1] + " kicked ";
-    } else
+	else if (cmd.size() == 3)
+        reason = cmd[2] + " kicked ";
+    else
 		throw ExceptionCode(ERR_NEEDMOREPARAMS);
+
     User *target = NULL;
-    int checkTarget = findTarget(cmd[1], cmd[2], allChannels, target);
+    std::vector<Channel*>::iterator it = findChannel(allChannels, cmd[1]);
+	if (it == allChannels.end())
+		throw ExceptionCode(ERR_NOSUCHCHANNEL); 
+
+    int checkTarget = findTarget(cmd[2], it, target);
     if (checkTarget == 0)
         serverReply(fd_origin, "353 " + user->getUsername() + " " +cmd[2] + "Empty channel", 0);   
-    else if(checkTarget == 3)
+    else if (checkTarget == 3)
 		throw ExceptionCode(ERR_NOSUCHCHANNEL); 
-    else if(checkTarget == 2)
+    else if (checkTarget == 2)
 		throw ExceptionCode(ERR_USERNOTINCHANNEL);
     else {
+
         User *userTmp;
-        int checkOp = findTarget(user->getUsername(), cmd[2], allChannels, userTmp);
+        int checkOp = findTarget(user->getUsername(), it, userTmp);
         if(checkOp == 2)
 			throw ExceptionCode(ERR_NOTONCHANNEL);
         else if(checkOp != 4)
 			throw ExceptionCode(ERR_CHANOPRIVSNEEDED);
-        else {
-            eraseUser(cmd[2], allChannels, (*target));
-            serverReply(fd_origin, "KICK " + cmd[2] + " " + (*target).getNickname() + " :" + reason, 0);
+        else
+		{
+			sendToUsers(it, user, cmd[2], reason);
+			(*it)->removeUserFromChannel(*target);
         }
     }
 }
